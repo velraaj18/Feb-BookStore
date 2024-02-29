b0c25d3fddddcd91b2f2a5da968b3586e9893f15
VELRAAJ A S <velraaj30@gmail.com>
2024-02-29 21:29:41 +0530

    books

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,51 @@ import express from "express";
 import { PORT } from "./config.js";
 import { mongoDb } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+// To post a new Book
+app.post("/books", async (req, res) => {
+  try {
+    const newBook = req.body;
+    const book = await Book.create(newBook);
+    return res.status(201).send({ book });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Error: " + error);
+  }
+});
+
+// to get all the books
+app.get("/books", async (req, res) => {
+  try {
+    const book = await Book.find({});
+    const count = book.length;
+    return res.status(200).send({
+      count,
+      book,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("No book found");
+  }
+});
+
+// To get a specific book using ID
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      res.status(404).send("No book found");
+    }
+    return res.status(200).send(book);
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("NO book found");
+  }
 });
 
 mongoose
